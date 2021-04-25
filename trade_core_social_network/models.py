d8e29a032886e48706d43667641174f64578ef64
Adrien Tchounkeu <adrientchounkeu10@gmail.com>
2021-04-25 16:31:37 +0100

    # adding a README.rst file # creating the models.py file # applying migrations on db.sqlite3

---
+++ trade_core_social_network/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    geolocation_data = models.IPAddressField()
+    holiday = models.JSONField(blank=True, null=True)
+    token = models.CharField(max_length=300)
+
+
+class Post(models.Model):
+    id_post = models.CharField(max_length=100, primary_key=True)
+    text = models.CharField(max_length=500)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.ManyToManyField(User, through='ViewPost')
+    likes = models.ManyToManyField(User, through='LikePost')
+    unlikes = models.ManyToManyField(User, through='UnLikePost')
+
+
+class ViewPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date_viewed = models.DateTimeField()
+    reason_viewed = models.CharField(max_length=100)
+
+
+class LikePost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date_liked = models.DateTimeField()
+    like_raison = models.CharField(max_length=100)
+
+
+class UnLikePost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date_unliked = models.DateTimeField()
+    unlike_raison = models.CharField(max_length=100)
